befb4c809ded03f3a03c5ab4889c89a74a4de3f1
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-08-17 23:08:47 +0530

    Added hamburger for menu to the navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,40 @@
+import Image from "next/image";
 import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const Navbar = () => {
-  return <div className="font-bold text-xl mt-3">Navbar</div>;
+  return (
+    <>
+      <nav className="bg-red-500 h-20 flex justify-between sm:ml-60">
+        <div className="flex sm:hidden">
+          <Sheet>
+            <SheetTrigger className="mx-2">
+              <Image
+                src="/assets/icons/hamburger.svg"
+                alt="menu"
+                width={20}
+                height={20}
+              />
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Filmpire</SheetTitle>
+                <SheetDescription>Sidebar</SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </div>
+        Navbar
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
