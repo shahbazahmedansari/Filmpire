e04ec2a5be1e610f298c699ebefe18ce284c1497
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-08-27 22:57:21 +0530

    Updated movie card component

--- components/shared/MovieCard.tsx
+++ components/shared/MovieCard.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface MovieCardProps {
@@ -9,10 +11,31 @@ interface MovieCardProps {
 const MovieCard = ({ key, index, movie }: MovieCardProps) => {
   return (
     <div
-      className="font-bold text-slate-900
+      className="text-slate-900 font-bold
     mt-3 mb-0 text-center overflow-hidden sm:text-ellipsis w-60 whitespace-nowrap"
     >
-      {movie.title}
+      <div
+        className="flex-grow hover:transition hover:duration-150 hover:ease-out hover:cursor-pointer"
+        key={key}
+      >
+        <Link
+          href={`/movie/${movie.id}`}
+          className="items-center max-sm:flex max-sm:flex-col"
+        >
+          {/* <Image
+            src={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                : "https://www.fillmurray.com/200/300"
+            }
+            alt={movie.title}
+            width={200}
+            height={300}
+            className="rounded-xl mb-3 hover:scale-105"
+          /> */}
+        </Link>
+        {movie.title}
+      </div>
     </div>
   );
 };
