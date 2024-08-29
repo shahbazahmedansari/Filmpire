517ee158cc6256b1b3dc9f2fef126b8ee72025cb
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-08-29 21:49:31 +0530

    Completed sidebar and mobile nav sheetbar

--- components/Navbar/MobileNav.tsx
+++ components/Navbar/MobileNav.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { SheetTrigger, SheetContent, Sheet } from "../ui/sheet";
 import Image from "next/image";
 import Link from "next/link";
+import { getGenres } from "@/lib/actions";
+import genreIcons from "../../public/assets/genres";
 
 const categories = [
   { label: "Popular", value: "popular" },
@@ -9,17 +11,8 @@ const categories = [
   { label: "Upcoming", value: "upcoming" },
 ];
 
-const demoCategories = [
-  "Action",
-  "Comedy",
-  "Crime",
-  "Drama",
-  "Mystery",
-  "Romance",
-  "Thriller",
-];
-
-const MobileNav = () => {
+const MobileNav = async () => {
+  const genreData = await getGenres();
   return (
     <div className="flex sm:hidden">
       <Sheet>
@@ -31,7 +24,7 @@ const MobileNav = () => {
             height={50}
           />
         </SheetTrigger>
-        <SheetContent className="border-none">
+        <SheetContent className="border-none overflow-y-scroll sm:hidden">
           <Link href="/" className="flex justify-center">
             <Image
               src="/assets/images/logo.png"
@@ -51,19 +44,41 @@ const MobileNav = () => {
               <div className="flex justify-start text-slate-700 m-3">
                 <h1>Categories</h1>
               </div>
-              {categories.map((item) => (
-                <div key={item.label} className="m-3">
-                  {item.label}
-                </div>
+              {categories.map((item: { label: string; value: string }) => (
+                <Link href={"/"} key={item.label}>
+                  <div className="m-3 flex items-center">
+                    <Image
+                      // @ts-ignore
+                      src={genreIcons[item.label.toLowerCase()]}
+                      // @ts-ignore
+                      alt={genreIcons[item.label.toLowerCase()]}
+                      width={30}
+                      height={30}
+                      className="mr-5"
+                    />
+                    <p>{item.label}</p>
+                  </div>
+                </Link>
               ))}
               <div className="border-t-2 border-slate-300 border-solid w-full"></div>
               <div className="flex justify-start text-slate-700 m-3">
                 <h1>Genres</h1>
               </div>
-              {demoCategories.map((item) => (
-                <div key={item} className="m-3">
-                  {item}
-                </div>
+              {genreData.genres.map((item: { name: string; id: number }) => (
+                <Link key={item.id} href="/">
+                  <div className="m-3 flex items-center">
+                    <Image
+                      // @ts-ignore
+                      src={genreIcons[item.name.toLowerCase()]}
+                      // @ts-ignore
+                      alt={genreIcons[item.name.toLowerCase()]}
+                      width={30}
+                      height={30}
+                      className="mr-5"
+                    />
+                    <p>{item.name}</p>
+                  </div>
+                </Link>
               ))}
             </div>
           </section>
--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,5 +1,8 @@
 import { getGenres } from "@/lib/actions";
 import React from "react";
+import genreIcons from "../../public/assets/genres";
+import Link from "next/link";
+import Image from "next/image";
 
 const categories = [
   { label: "Popular", value: "popular" },
@@ -7,42 +10,53 @@ const categories = [
   { label: "Upcoming", value: "upcoming" },
 ];
 
-const demoCategories = [
-  "Action",
-  "Comedy",
-  "Crime",
-  "Drama",
-  "Mystery",
-  "Romance",
-  "Thriller",
-];
-
 const Sidebar = async () => {
   const genreData = await getGenres();
-  console.log(genreData.genres);
   return (
     <section
-      className="w-[240px] min-h-screen hidden sm:flex overflow-y-scroll mt-20 
+      className="w-[240px] min-h-screen hidden sm:flex overflow-scroll mt-20 
     justify-center border-r-2"
     >
-      <div className="flex flex-col w-full">
+      <div className="flex flex-col w-full overflow-scroll">
         <div className="border-t-2 border-slate-300 border-solid w-full"></div>
         <div className="flex justify-start text-slate-700 m-3">
           <h1>Categories</h1>
         </div>
-        {categories.map((item) => (
-          <div key={item.label} className="m-3">
-            {item.label}
-          </div>
+        {categories.map((item: { label: string; value: string }) => (
+          <Link href={"/"} key={item.label}>
+            <div className="m-3 flex items-center">
+              <Image
+                // @ts-ignore
+                src={genreIcons[item.label.toLowerCase()]}
+                // @ts-ignore
+                alt={genreIcons[item.label.toLowerCase()]}
+                width={30}
+                height={30}
+                className="mr-5"
+              />
+              <p>{item.label}</p>
+            </div>
+          </Link>
         ))}
         <div className="border-t-2 border-slate-300 border-solid w-full"></div>
         <div className="flex justify-start text-slate-700 m-3">
           <h1>Genres</h1>
         </div>
         {genreData.genres.map((item: { name: string; id: number }) => (
-          <div key={item.id} className="m-3">
-            {item.name}
-          </div>
+          <Link key={item.id} href="/">
+            <div className="m-3 flex items-center">
+              <Image
+                // @ts-ignore
+                src={genreIcons[item.name.toLowerCase()]}
+                // @ts-ignore
+                alt={genreIcons[item.name.toLowerCase()]}
+                width={30}
+                height={30}
+                className="mr-5"
+              />
+              <p>{item.name}</p>
+            </div>
+          </Link>
         ))}
       </div>
     </section>
